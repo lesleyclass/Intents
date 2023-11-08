295de35b4e4d93f3a3e1ed555f8fd9c00441e085
lesley.ferreira <lesley.ferreira.tw@naturapay.net>
2023-11-07 21:12:20 -0300

    refactor: refector main activity

--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -19,6 +19,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityMainBinding
@@ -28,26 +29,32 @@ open class MainActivity : AppCompatActivity() {
     private val activityMainBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    private val parameterResultLauncher: ActivityResultLauncher<Intent> by lazy {
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+    private lateinit var parameterResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var callPhonePermission: ActivityResultLauncher<String>
+    private lateinit var pickImageActivityResult: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityMainBinding.root)
+        setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
+        supportActionBar?.subtitle = localClassName
+
+        parameterResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
                 result?.data?.getStringExtra(PARAMETER_EXTRA).also {
                     activityMainBinding.parameterTv.text = it
                 }
             }
         }
-    }
-    private val callPhonePermission: ActivityResultLauncher<String> by lazy {
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { premissionGranted ->
-            if (premissionGranted) {
+        callPhonePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissionGranted ->
+            if (permissionGranted) {
                 callPhone(true)
             } else {
-                Toast.makeText(this, "Permission Deined", Toast.LENGTH_LONG).show()
+                val toastText = getText(R.string.permission_denied_text)
+                Toast.makeText(this, toastText, Toast.LENGTH_LONG).show()
             }
         }
-    }
-    private val pickImageActivityResult: ActivityResultLauncher<Intent> by lazy {
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        pickImageActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             with(result) {
                 if (resultCode == RESULT_OK) {
                     data?.data?.also {
@@ -59,13 +66,6 @@ open class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(activityMainBinding.root)
-        setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
-        supportActionBar?.subtitle = localClassName
 
         activityMainBinding.apply {
             parameterBt.setOnClickListener {
@@ -73,8 +73,7 @@ open class MainActivity : AppCompatActivity() {
                     Intent(this@MainActivity, ParameterActivity::class.java).apply {
                         putExtra(PARAMETER_EXTRA, parameterTv.text.toString())
                     }
-
-                startActivityForResult(parameterIntent, PARAMETER_REQUEST_CODE)
+                parameterResultLauncher.launch(parameterIntent)
             }
         }
     }
@@ -93,60 +92,79 @@ open class MainActivity : AppCompatActivity() {
         return true
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.openActivityMi -> {
-                val parameterIntent = Intent(OPEN_PARAMETER_ACTIVITY).apply {
-                    putExtra(PARAMETER_EXTRA, activityMainBinding.parameterTv.text.toString())
-                }
-                parameterResultLauncher.launch(parameterIntent)
+                openActivity()
                 true
             }
-
             R.id.viewMi -> {
-                val browserIntent = browserIntent()
-                startActivity(browserIntent)
+                handleView()
                 true
             }
-
             R.id.callMi -> {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED) {
-                        callPhone(true)
-                    } else {
-                        callPhonePermission.launch(CALL_PHONE)
-                    }
-                } else {
-                    callPhone(true)
-                }
+                handleCall()
                 true
             }
-
             R.id.dialMi -> {
                 callPhone(false)
                 true
             }
-
             R.id.pickMi -> {
-                val imageDir = Environment
-                    .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-                    .path
-                pickImageActivityResult.launch(
-                    Intent(ACTION_PICK).apply { setDataAndType(Uri.parse(imageDir), PICTURE_TYPE) }
-                )
+                handlePick()
                 true
             }
-
             R.id.chooserMi -> {
-                startActivity(Intent(ACTION_CHOOSER).apply {
-                    val value = getString(R.string.choose_text)
-                    putExtra(EXTRA_TITLE, value)
-                    putExtra(EXTRA_INTENT, browserIntent())
-                })
+                handleChooser()
                 true
             }
-
             else -> { false }
+        }
+    }
+
+    private fun openActivity() {
+        val parameterIntent = Intent(OPEN_PARAMETER_ACTIVITY_ACTION).apply {
+            putExtra(PARAMETER_EXTRA, activityMainBinding.parameterTv.text.toString())
+        }
+        parameterResultLauncher.launch(parameterIntent)
+    }
+
+    private fun handleView() {
+        val browserIntent = browserIntent()
+        startActivity(browserIntent)
+    }
+
+    private fun handleChooser() {
+        startActivity(Intent(ACTION_CHOOSER).apply {
+            val value = getString(R.string.choose_text)
+            putExtra(EXTRA_TITLE, value)
+            putExtra(EXTRA_INTENT, browserIntent())
+        })
+    }
+
+    private fun handlePick() {
+        val imageDir = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            .path
+        pickImageActivityResult.launch(
+            Intent(ACTION_PICK).apply { setDataAndType(Uri.parse(imageDir), PICTURE_TYPE) }
+        )
+    }
+
+    private fun handleCall() {
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> { call() }
+            else -> { callPhone(true) }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun call() {
+        if (hasCallPhonePermission()) {
+            callPhone(true)
+        } else {
+            callPhonePermission.launch(CALL_PHONE)
         }
     }
 
@@ -164,9 +182,13 @@ open class MainActivity : AppCompatActivity() {
         })
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun hasCallPhonePermission(): Boolean =
+        checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED
+
     companion object {
         private const val PARAMETER_REQUEST_CODE = 0
-        private const val OPEN_PARAMETER_ACTIVITY = "OPEN_PARAMETER_ACTIVITY"
-        private const val PICTURE_TYPE = "\"image/*\""
+        private const val OPEN_PARAMETER_ACTIVITY_ACTION = "OPEN_PARAMETER_ACTIVITY_ACTION"
+        private const val PICTURE_TYPE = "image/*"
     }
 }
